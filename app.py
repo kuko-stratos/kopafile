2db800cebe2797f78961b5c263c8ebd82612a911
kuko-stratos <157869439+kuko-stratos@users.noreply.github.com>
2024-01-29 18:18:48 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,25 +1,16 @@
 import streamlit as st
 
-st.title("計算アプリ")
+st.title("あと何分かかんのこれ？")
 
 # input
-a = st.number_input("数字1")
-b = st.number_input("数字2")
-
-method = st.sidebar.selectbox("計算方法",["+","-","×","÷"])
+a = st.number_input("前に何人並んでる？")
+b = st.number_input("今の1分間で何人うしろに並んだ？")
 
 # process
-if method == "+":
-  c = a + b
-elif method == "-":
-  c = a - b
-elif method == "×":
-  c = a * b
-elif method == "÷":
-  c = a / b
-else:
-  pass
+c = a / b
 
 # output
-st.title(f"{method}結果")
+# st.title("計算結果")
+st.caption("たぶん")
 st.text(c)
+st.caption("分後ぐらいに順番がまわってくるでしょう。知らんけど。")
