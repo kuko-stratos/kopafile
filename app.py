44ad15c51a6c56265518cea49a2551b21e4ae198
kuko-stratos <157869439+kuko-stratos@users.noreply.github.com>
2024-01-29 19:46:44 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -3,14 +3,22 @@ import streamlit as st
 st.title("あと何分かかんのこれ？")
 
 # input
-a = st.number_input("前に何人並んでる？")
-b = st.number_input("今の1分間で何人うしろに並んだ？")
+a = st.number_input("前に何人並んでる？",0)
+b = st.number_input("今の1分間で何人うしろに並んだ？",0)
 
 # process
-c = a / b
+try:
+  c = a / b
 
 # output
 # st.title("計算結果")
-st.caption("たぶん")
-st.text(c)
-st.caption("分後ぐらいに順番がまわってくるでしょう。知らんけど。")
+  st.caption("たぶん")
+  st.text(c)
+  st.caption("分後ぐらいに順番がまわってくるでしょう。知らんけど。")
+
+except ValueError:
+  print('整数を入力してください')
+
+except ZeroDivisionError:
+  print('0を入力しないでください')
+
